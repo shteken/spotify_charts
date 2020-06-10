f1c51bc414df1772864f7fa9e3082cbb2ff8459c
shteken <barshteken@gmail.com>
2020-06-10 12:11:18 +0300

    added check if DAG should run (#2)
    * add check if DAG should be run
    
    * fix syntax
    
    * change to check task
    
    * added error handling flow

--- spotify.py
+++ spotify.py
@@ -3,6 +3,8 @@ import datetime
 import airflow
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.operators.python_operator import BranchPythonOperator
+# from airflow.contrib.operators.bigquery_check_operator import BigQueryCheckOperator
 
 import pandas as pd
 from dependencies.spotify_charts_to_json import create_json
@@ -17,7 +19,7 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': datetime.timedelta(minutes=5),
-    'start_date': datetime.datetime(2020, 6, 7)
+    'start_date': datetime.datetime(2020, 6, 8)
 }
 
 dag = airflow.DAG(
@@ -25,6 +27,43 @@ dag = airflow.DAG(
     default_args=default_args,
     catchup=False,
     schedule_interval='0 9 * * * ' # run every day at 09:00 UTC
+)
+
+sql_check_if_date_exists = """
+    SELECT
+    CASE
+        WHEN
+            EXISTS ( SELECT date FROM `valiant-nucleus-162210.spotify.fact_music_chart` WHERE date = DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
+        THEN 
+            true
+        ELSE
+            false
+    END
+        doesDateExist
+    """ # true if there are records, false if there are no records
+
+# this method didnt work for me, got 404 from BigQuery:
+"""check_if_need_to_run = BigQueryCheckOperator( 
+    task_id='check_if_date_exists',
+    sql=sql_check_if_date_exists,
+    use_legacy_sql=False,
+    dag=dag,
+    depends_on_past=False
+)"""
+
+# so I used pandas and PythonOperator for this task
+def check_if_date_exists():
+    if pd.read_gbq(query = sql_check_if_date_exists, dialect = 'standard').bool(): # return bool value from the query
+        return 'do_nothing'
+    else:
+        return 'create_json'
+        #raise Exception("date already exists")
+
+check_if_need_to_run = BranchPythonOperator(
+    task_id='check_if_date_exists',
+    python_callable=check_if_date_exists,
+    dag=dag,
+    depends_on_past=False
 )
 
 def call_for_raw():
@@ -47,22 +86,26 @@ t2 = PythonOperator(
     depends_on_past=False
 )
 
-sql = """
-    SELECT
-        PARSE_DATE('%Y%m%d',
-            SUBSTR(_FILE_NAME, -12, 8)) date,
-        country,
-        position,
-        name,
-        SUBSTR(artist, 4) artist,
-        streams
-    FROM
-        `valiant-nucleus-162210.spotify.music_chart`
-    WHERE
-        SUBSTR(_FILE_NAME, -12, 8) = FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
+insert_to_fact = """
+        SELECT
+            PARSE_DATE('%Y%m%d',
+                SUBSTR(m._FILE_NAME, -12, 8)) date,
+            c.CountryFullName country,
+            position,
+            name,
+            SUBSTR(artist, 4) artist,
+            streams
+        FROM
+            `valiant-nucleus-162210.spotify.music_chart` m
+        JOIN
+            `valiant-nucleus-162210.spotify.countries` c
+        ON
+            m.country = c.country
+        WHERE
+            SUBSTR(m._FILE_NAME, -12, 8) = FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   """
 t3 = BigQueryOperator(
-    sql=sql,
+    sql=insert_to_fact,
     destination_dataset_table= 'valiant-nucleus-162210.spotify.fact_music_chart',
     task_id='insert_to_fact',
     write_disposition='WRITE_APPEND',
@@ -70,4 +113,15 @@ t3 = BigQueryOperator(
     dag=dag
 )
 
-t1 >> t2 >> t3
+def date_already_exists():
+    print('date already exists in BigQuery')
+
+
+do_nothing = PythonOperator(
+    task_id='do_nothing',
+    python_callable=date_already_exists,
+    dag=dag
+)
+
+check_if_need_to_run >> t1 >> t2 >> t3 # the process should run
+check_if_need_to_run >> do_nothing # date already exists, process should not run
